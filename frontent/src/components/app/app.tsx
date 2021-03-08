8ab5b10ecf826a2b3bdb600fc760e36b900291ff
Miloky <monmorti@gmail.com>
2021-03-08 07:59:39 +0200

    Refactoring

--- frontent/src/components/app/app.tsx
+++ frontent/src/components/app/app.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, ReactElement } from 'react';
 
 const App: FunctionComponent = (): ReactElement => {
-  return <div>test</div>;
+  return <div />
 };
 
 export default App;
