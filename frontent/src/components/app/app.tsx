a97f257cf448d2de9b150a5bbc838bdb7963624d
Miloky <monmorti@gmail.com>
2021-03-08 07:58:12 +0200

    Initial frontend

---
+++ frontent/src/components/app/app.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent, ReactElement } from 'react';
+
+const App: FunctionComponent = (): ReactElement => {
+  return <div>test</div>;
+};
+
+export default App;
---
+++ frontent/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+
+// TODO: Tests
+// TODO: WebVitals
+// TODO: Eslint
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
